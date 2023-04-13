5ebab9465d29becb111b18bc8ca7c4d4ee03dd54
mkmutaki <mkmutaki@gmail.com>
2023-04-13 17:51:19 +0300

    GH pages fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         background: "hsl(216, 12%, 8%)",
         "card-bg": "hsl(213, 19%, 18%)",
-        "circle-focus": "hsl(217, 12%, 63%)",
+        "circle-focus": "hsl(217, 12%, 64%)",
         "small-text": "hsl(217, 12%, 63%)",
       },
       fontFamily: {
